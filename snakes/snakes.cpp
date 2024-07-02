8ecce3aeaaeecb4497377050b6f3166917c3306a
Gurjiwan Singh <gurrjiwan@gmail.com>
2024-07-02 14:55:10 +0530

    Issue in using conio and windiows.h as they are not standard c libraries. need cross-platform libraries

---
+++ snakes/snakes.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+//#include<conio.h>  not a part of standard c library
+//#include<windows.h> not a part of standard c library needs to be replaced
+
+int main()
+{
+	printf("Hello World. \n");
+	return 0;
+}
+
