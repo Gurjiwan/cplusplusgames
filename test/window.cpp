135ae8af0b6cdf0f2220c2f7fa00c11a4c0a270d
Gurjiwan Singh <gurrjiwan@gmail.com>
2024-07-07 01:25:18 +0530

    Making a test Window using glfw library

---
+++ test/window.cpp
@@ -0,0 +1,37 @@
+#include<GLFW/glfw3.h>
+
+int main(void)
+{
+	GLFWwindow* testWindow;
+
+	// Initialize library by calling glfwInit()
+	if (!glfwInit())
+		return -1;
+
+	//Create a window
+	testWindow = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+	if(!testWindow)
+	{
+		glfwTerminate();
+		return -1;
+	}
+	// Make it current context
+	glfwMakeContextCurrent(testWindow);
+
+	// Loop until user closes the Window
+	while(!glfwWindowShouldClose(testWindow))
+	{
+		//Render Here
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Swap front and back buffers
+		glfwSwapBuffers(testWindow);
+
+		//Polling for process and events
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
